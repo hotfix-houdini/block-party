12da0d3882fbe2f066ed03fede646a71ea2d2006
hotfix-houdini <70034632+hotfix-houdini@users.noreply.github.com>
2024-04-17 07:31:21 -0500

    BeamBlockSettings with optional OmitIncompleteFinalWindow functionality

---
+++ src/BlockParty/Blocks/Beam/BeamBlockSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace BlockParty.Blocks.Beam
+{
+    public class BeamBlockSettings
+    {
+        /// <summary>
+        /// When BeamBlock is completed it might have a partial window accumulated.<br/>
+        /// This property, when FALSE, will emit that final - potentially partial - window on completition.<br/>
+        /// When TRUE, the final - potentially partial - window will NOT be POSTed to downstream blocks and will be discarded.
+        /// </summary>
+        public bool OmitIncompleteFinalWindow { get; set; }
+    }
+}
