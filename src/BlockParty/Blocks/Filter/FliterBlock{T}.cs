de29d99ecb4310f5abb3095fefbc8db291c0562e
hotfix-houdini <70034632+hotfix-houdini@users.noreply.github.com>
2024-05-07 07:26:49 -0500

    filter block

---
+++ src/BlockParty/Blocks/Filter/FliterBlock{T}.cs
@@ -0,0 +1,103 @@
+﻿using BlockParty.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace BlockParty.Blocks.Filter
+{
+    /// <summary>
+    /// FliterBlock allows you to filter down a stream by any lambda method.<br/>
+    /// 
+    /// <param name="predicate">The lambda used to gatekeep the downstream.</param><br/><br/>
+    /// 
+    /// See <a href="https://github.com/hotfix-houdini/block-party">GitHub</a> for more details.
+    /// </summary>
+    public class FliterBlock<T> : IPropagatorBlock<T, T>, IReceivableSourceBlock<T>
+    {
+        public delegate bool Predicate(T item);
+
+        private readonly ITargetBlock<T> m_target;
+        private readonly IReceivableSourceBlock<T> m_source;
+
+        /// <summary>
+        /// FliterBlock allows you to filter down a stream by any lambda method.<br/>
+        /// 
+        /// <param name="predicate">The lambda used to gatekeep the downstream.</param><br/><br/>
+        /// 
+        /// See <a href="https://github.com/hotfix-houdini/block-party">GitHub</a> for more details.
+        /// </summary>
+        public FliterBlock(Predicate predicate)
+        {
+            var source = new BufferBlock<T>();
+            var target = new ActionBlock<T>(item =>
+            {
+                if (predicate(item))
+                {
+                    var posted = source.Post(item);
+                    if (!posted)
+                    {
+                        throw new FailedToPostException();
+                    }
+                }
+            });
+
+            target.Completion.ContinueWith(delegate
+            {
+                source.Complete();
+            });
+
+            m_target = target;
+            m_source = source;
+        }
+
+        public Task Completion { get { return m_source.Completion; } }
+
+        public void Complete()
+        {
+            m_target.Complete();
+        }
+
+        public T ConsumeMessage(DataflowMessageHeader messageHeader, ITargetBlock<T> target, out bool messageConsumed)
+        {
+            return m_source.ConsumeMessage(messageHeader, target, out messageConsumed);
+        }
+
+        public void Fault(Exception exception)
+        {
+            m_target.Fault(exception);
+        }
+
+        public IDisposable LinkTo(ITargetBlock<T> target, DataflowLinkOptions linkOptions)
+        {
+            return m_source.LinkTo(target, linkOptions);
+        }
+
+        public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, T messageValue, ISourceBlock<T> source, bool consumeToAccept)
+        {
+            return m_target.OfferMessage(messageHeader, messageValue, source, consumeToAccept);
+        }
+
+        public void ReleaseReservation(DataflowMessageHeader messageHeader, ITargetBlock<T> target)
+        {
+            m_source.ReleaseReservation(messageHeader, target);
+        }
+
+        public bool ReserveMessage(DataflowMessageHeader messageHeader, ITargetBlock<T> target)
+        {
+            return m_source.ReserveMessage(messageHeader, target);
+        }
+
+        public bool TryReceive(Predicate<T> filter, out T item)
+        {
+            return m_source.TryReceive(filter, out item);
+        }
+
+        public bool TryReceiveAll(out IList<T> items)
+        {
+            var reuslt = m_source.TryReceiveAll(out items);
+            items = items ?? new List<T>();
+            return reuslt;
+        }
+    }
+}
---
+++ tests/BlockParty.Tests/Blocks/FilterBlockTests.cs
@@ -0,0 +1,58 @@
+﻿using BlockParty.Blocks.Filter;
+using System.Threading.Tasks.Dataflow;
+
+namespace BlockParty.Tests.Blocks;
+public class FilterBlockTests
+{
+    [Test]
+    [TestCase("1, 2, 3", 0, "1, 2, 3")]
+    [TestCase("1, 2, 3", 1, "2, 3")]
+    [TestCase("1, 2, 3", 2, "3")]
+    [TestCase("1, 2, 3", 3, "")]
+    [TestCase("1, 2, 3", -1, "1, 2, 3")]
+    public async Task ShouldFilter(string inputStream, int lowerBound, string expectedOutputStream)
+    {
+        // arrange
+        var inputs = inputStream.Split(", ").Select(x => int.Parse(x)).ToList();
+        var expectedOutputs = expectedOutputStream.Split(", ").Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x)).ToList();
+        var filterBlock = new FliterBlock<int>(x => x > lowerBound);
+
+        var actualOutputs = new List<int>();
+        var outputCollector = new ActionBlock<int>(x => actualOutputs.Add(x));
+
+        filterBlock.LinkTo(outputCollector, new DataflowLinkOptions() {  PropagateCompletion = true });
+
+        // act
+        foreach (var input in inputs)
+        {
+            filterBlock.Post(input);
+        }
+        filterBlock.Complete();
+        await outputCollector.Completion;
+
+        // assert
+        CollectionAssert.AreEqual(expectedOutputs, actualOutputs);
+    }
+
+    [Test]
+    public async Task ShouldFilterSimple()
+    {
+        // arrange
+        var filterBlock = new FliterBlock<int>(x => x % 2 == 0); // even #'s only
+
+        var actualOutputs = new List<int>();
+        var outputCollector = new ActionBlock<int>(x => actualOutputs.Add(x));
+        filterBlock.LinkTo(outputCollector, new DataflowLinkOptions() { PropagateCompletion = true });
+
+        // act
+        for (int i = 0; i < 10; i++)
+        {
+            filterBlock.Post(i);
+        }
+        filterBlock.Complete();
+        await outputCollector.Completion;
+
+        // assert
+        CollectionAssert.AreEqual(new List<int>() { 0, 2, 4, 6, 8}, actualOutputs);
+    }
+}
