644667e78c92aa76397940b3b320a271765c7c0e
hotfix-houdini <70034632+hotfix-houdini@users.noreply.github.com>
2024-08-31 12:46:39 -0500

    chatgpt refactor

--- src/BlockParty/Builder/DataflowBuilder.cs
+++ src/BlockParty/Builder/DataflowBuilder.cs
@@ -1,58 +1,11 @@
 ﻿using BlockParty.Blocks.Filter;
 using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks.Dataflow;
 
 namespace BlockParty.Builder
 {
-    public class DataflowBuilder<TCurrentType> : DataflowBuilder<TCurrentType, TCurrentType>
-    {
-        public DataflowBuilder() : base(new BufferBlock<TCurrentType>())
-        {
-        }
-
-        public new DataflowBuilder<TCurrentType, TNewType> Select<TNewType>(Func<TCurrentType, TNewType> lambda)
-        {
-            var newBlock = new TransformBlock<TCurrentType, TNewType>(lambda);
-
-            if (_lastBlock == null)
-            {
-                _sourceBlock.LinkTo(newBlock, _linkOptions);
-            }
-            else
-            {
-                _lastBlock.LinkTo(newBlock, _linkOptions);
-            }
-
-            return new DataflowBuilder<TCurrentType, TNewType>(_sourceBlock, newBlock);
-        }
-
-        public new IPropagatorBlock<TCurrentType, TCurrentType> Build()
-        {
-            if (_lastBlock == null)
-            {
-                return _sourceBlock;
-            }
-            else
-            {
-                return DataflowBlock.Encapsulate<TCurrentType, TCurrentType>(_sourceBlock, _lastBlock);
-            }
-        }
-
-        protected override void AddBlock(IPropagatorBlock<TCurrentType, TCurrentType> newBlock)
-        {
-            if (_lastBlock == null)
-            {
-                _sourceBlock.LinkTo(newBlock, _linkOptions);
-                _lastBlock = newBlock;
-            }
-            else
-            {
-                _lastBlock.LinkTo(newBlock, _linkOptions);
-                _lastBlock = newBlock;
-            }
-        }
-    }
-
     public class DataflowBuilder<TOriginalType, TCurrentType>
     {
         protected readonly BufferBlock<TOriginalType> _sourceBlock;
@@ -63,7 +16,8 @@ namespace BlockParty.Builder
             PropagateCompletion = true
         };
 
-        public DataflowBuilder(BufferBlock<TOriginalType> originalBlock) : this(originalBlock, null)
+        public DataflowBuilder()
+            : this(new BufferBlock<TOriginalType>(), null)
         {
         }
 
@@ -73,6 +27,13 @@ namespace BlockParty.Builder
             _lastBlock = currentBlock;
         }
 
+        public DataflowBuilder<TOriginalType, TNewType> Select<TNewType>(Func<TCurrentType, TNewType> lambda)
+        {
+            var newBlock = new TransformBlock<TCurrentType, TNewType>(lambda);
+            AddBlock(newBlock);
+            return new DataflowBuilder<TOriginalType, TNewType>(_sourceBlock, newBlock);
+        }
+
         public DataflowBuilder<TOriginalType, TCurrentType> Where(FliterBlock<TCurrentType>.Predicate predicate)
         {
             var newBlock = new FliterBlock<TCurrentType>(predicate);
@@ -80,35 +41,47 @@ namespace BlockParty.Builder
             return this;
         }
 
-        public DataflowBuilder<TOriginalType, TCurrentType> Select(Func<TCurrentType, TCurrentType> lambda)
-        {
-            var newBlock = new TransformBlock<TCurrentType, TCurrentType>(lambda);
-            AddBlock(newBlock);
-            return this;
-        }
-
-        public DataflowBuilder<TOriginalType, TNewType> Select<TNewType>(Func<TCurrentType, TNewType> lambda)
-        {
-            var newBlock = new TransformBlock<TCurrentType, TNewType>(lambda);
-            _lastBlock.LinkTo(newBlock, _linkOptions); // make addblock?
-            return new DataflowBuilder<TOriginalType, TNewType>(_sourceBlock, newBlock);
-        }
-
         public IPropagatorBlock<TOriginalType, TCurrentType> Build()
         {
-            return DataflowBlock.Encapsulate<TOriginalType, TCurrentType>(_sourceBlock, _lastBlock);
+            return DataflowBlock.Encapsulate(_sourceBlock, _lastBlock);
         }
 
-        protected virtual void AddBlock(IPropagatorBlock<TCurrentType, TCurrentType> newBlock)
+        protected virtual void AddBlock<TNewType>(IPropagatorBlock<TCurrentType, TNewType> newBlock)
         {
             if (_lastBlock == null)
             {
-                _lastBlock = newBlock;
+                // Handle the case where the chain starts with the source block
+                var castedBlock = new TransformBlock<TOriginalType, TCurrentType>(x => (TCurrentType)(object)x);
+                _sourceBlock.LinkTo(castedBlock, _linkOptions);
+                castedBlock.LinkTo(newBlock, _linkOptions);
             }
             else
             {
+                // Link the last block to the new block
                 _lastBlock.LinkTo(newBlock, _linkOptions);
-                _lastBlock = newBlock;
+            }
+
+            // Update the last block to be the new block
+            _lastBlock = newBlock as ISourceBlock<TCurrentType>;
+        }
+    }
+
+    // Entry point for the builder
+    public class DataflowBuilder<T> : DataflowBuilder<T, T>
+    {
+        public DataflowBuilder() : base()
+        {
+        }
+
+        public new IPropagatorBlock<T, T> Build()
+        {
+            if (_lastBlock == null)
+            {
+                return _sourceBlock;
+            }
+            else
+            {
+                return DataflowBlock.Encapsulate(_sourceBlock, _lastBlock);
             }
         }
     }
