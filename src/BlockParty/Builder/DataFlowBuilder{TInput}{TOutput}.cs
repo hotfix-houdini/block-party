f267ab73680c95b3b40893288d0160264726236f
hotfix-houdini <70034632+hotfix-houdini@users.noreply.github.com>
2025-02-21 11:47:14 -0600

    remove experiemental fanout with join.

--- src/BlockParty/Builder/DataFlowBuilder{TInput}{TOutput}.cs
+++ src/BlockParty/Builder/DataFlowBuilder{TInput}{TOutput}.cs
@@ -39,40 +39,6 @@ namespace BlockParty.Builder
             return this;
         }
 
-        /* - introduce FanoutDataflowBuilder without
-         *      - Where (b/c that would break the join)
-         *      - Build (b/c we call that internally)
-         * - make method slightly more generic (generic helper method to reduce duplicated code. can't loop though.)
-         * - expose several overloads (2 chains, 3 chains, 4 chains, 5 chains)
-         * - lots of complicated tests
-         */
-        public DataflowBuilder<TInput, (TNewType1, TNewType2)> FanoutWithJoin<TNewType1, TNewType2>(
-            Func<DataflowBuilder<TOutput>, DataflowBuilder<TOutput, TNewType1>> chain1,
-            Func<DataflowBuilder<TOutput>, DataflowBuilder<TOutput, TNewType2>> chain2)
-        {
-            var fanoutBlock = new BroadcastBlock<TOutput>(x => x);
-            var joinBlock = new JoinBlock<TNewType1, TNewType2>();
-            var tuple2ToValueTupleConverter = new TransformBlock<Tuple<TNewType1, TNewType2>, (TNewType1, TNewType2)>(x => (x.Item1, x.Item2));
-
-            var newBuilder1 = new DataflowBuilder<TOutput>();
-            var chain1Builder = chain1(newBuilder1);
-            var chain1Block = chain1Builder.Build();
-
-            var newBuilder2 = new DataflowBuilder<TOutput>();
-            var chain2Builder = chain2(newBuilder2);
-            var chain2Block = chain2Builder.Build();
-
-            fanoutBlock.LinkTo(newBuilder1._sourceBlock, _linkOptions);
-            fanoutBlock.LinkTo(newBuilder2._sourceBlock, _linkOptions);
-            chain1Block.LinkTo(joinBlock.Target1, _linkOptions);
-            chain2Block.LinkTo(joinBlock.Target2, _linkOptions);
-            joinBlock.LinkTo(tuple2ToValueTupleConverter, _linkOptions);
-
-            var newBlock = DataflowBlock.Encapsulate(fanoutBlock, tuple2ToValueTupleConverter);
-            AddBlock(newBlock);
-            return new DataflowBuilder<TInput, (TNewType1, TNewType2)>(_sourceBlock, newBlock);
-        }
-
         public IPropagatorBlock<TInput, TOutput> Build()
         {
             return DataflowBlock.Encapsulate(_sourceBlock, _lastBlock);
