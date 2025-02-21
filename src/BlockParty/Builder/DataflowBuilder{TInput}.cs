ff29f2b3758c93a8e1bb0bc5fc137a8b1cd64b74
hotfix-houdini <70034632+hotfix-houdini@users.noreply.github.com>
2025-02-21 15:55:58 -0600

    Add some minor documentaiton

--- src/BlockParty/Builder/DataflowBuilder{TInput}.cs
+++ src/BlockParty/Builder/DataflowBuilder{TInput}.cs
@@ -2,6 +2,12 @@
 
 namespace BlockParty.Builder
 {
+    /// <summary>
+    /// Used to construct encapsulated block chains. <br/><br/>
+    /// Example: `new DateFlowBuilder<int>().Where(n => n % 2 == 0).Select(n => $"{n + 1}").Build()`, which makes a pipeline to filter to even numbers, then map to +1 as a string.
+    /// 
+    /// See <a href="https://github.com/hotfix-houdini/block-party">GitHub</a> for more details, examples, and tests.
+    /// </summary>
     public class DataflowBuilder<TInput> : DataflowBuilder<TInput, TInput>
     {
         public DataflowBuilder() : base(new BufferBlock<TInput>())
