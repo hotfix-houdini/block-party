29deb9886d169042bb08bfd93e2e1e6752620d21
hotfix-houdini <70034632+hotfix-houdini@users.noreply.github.com>
2025-02-27 11:44:17 -0600

    visualizer render + tests

---
+++ src/BlockParty/Visualizer/BlockNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace BlockParty.Visualizer;
+
+public record BlockNode
+{
+    public required string Name { get; init; }
+    public required string InputType { get; init; }
+    public required string OutputType { get; init; }
+
+    public static BlockNode Create<TInput, TOutput>(string name)
+    {
+        return new BlockNode()
+        {
+            Name = name,
+            InputType = ConstructTypeName<TInput>(),
+            OutputType = ConstructTypeName<TOutput>()
+        };
+    }
+
+    private static string ConstructTypeName<TType>()
+    {
+        var type = typeof(TType);
+        var typeName = type.Name;
+        if (type.IsGenericType)
+        {
+            var typeParameters = type.GetGenericArguments().ToList();
+            typeName = type.Name.Substring(0, type.Name.IndexOf('`')) + "&lt;" +
+                string.Join(",&nbsp;", Array.ConvertAll(type.GetGenericArguments(), t => t.Name)) + "&gt;";
+        }
+
+        return typeName;
+    }
+}
